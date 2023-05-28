b7460a3919d263681661af279fe9c8b492bca5db
JinsunKim-2021180006 <112993350+JinsunKim-2021180006@users.noreply.github.com>
2023-05-28 22:38:18 +0900

    복사기능 추가
    현재 가능 기능: 웹연결, 검색(2종류), 검색 내용 복사

--- PastSearch.py
+++ PastSearch.py
@@ -1,56 +1,54 @@
+#pip install clipboard
+import clipboard
 import requests
 from tkinter import *
 from tkinter.ttk import *
 import xml.etree.ElementTree as ET
-import main
+
+def parse_xml(xml_data):
+    result_dict = {}
+    root = ET.fromstring(xml_data)
+    items = root.find('body/items')
+    if items is not None:
+        for item in items.findall('item'):
+            astro_title = item.find('astroTitle')
+            astro_event = item.find('astroEvent')
+            if astro_title is not None and astro_event is not None:
+                result_dict[astro_title.text] = astro_event.text
+            else: 
+                result_dict['0'] = '0'
+    return result_dict
+
+def printData(DATA_DIC, printlist):
+    printlist.configure(state='normal')
+    printlist.delete("0.0","end")
+    if len(DATA_DIC) == 0:
+        printlist.insert("end","해당 달에 일어난 사건이 없습니다.")
+    else:
+        for title, event in DATA_DIC.items():
+            printlist.insert("end","사건 이름: ")
+            printlist.insert("end",title or "")
+            printlist.insert("end","\n")
+            printlist.insert("end","사건 내용: ")
+            printlist.insert("end",event or ""+"\n\n")
+        printlist.configure(state='disabled')
+
+def listcopy(printlist):
+    text_value = printlist.get("1.0", "end-1c")
+    clipboard.copy(text_value)
+
+def upload_xml(Year, month, printlist):
+    url = 'http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo'
+    service_key = "Nt9JLubbJypY+2kCOW3BhPuQjwuYm0o7eHRKSvBxk7aUk/CI9KgBzv06qHctRy26s56jTjqI0kdOKj1gy3oI8Q=="
+    queryParams = {'serviceKey' : service_key, 'solYear' : Year.get(), 'solMonth' : month.get()}
+    response = requests.get(url, params=queryParams)
+    xmldata = response.content
+
+    Data_Dic = parse_xml(xmldata)
+    printData(Data_Dic, printlist)
 
 def pastsearch():
-    def parse_xml(xml_data):
-        result_dict = {}
-        root = ET.fromstring(xml_data)
-        items = root.find('body/items')
-        if items is not None:
-            for item in items.findall('item'):
-                astro_title = item.find('astroTitle')
-                astro_event = item.find('astroEvent')
-                if astro_title is not None and astro_event is not None:
-                    result_dict[astro_title.text] = astro_event.text
-                else: 
-                    result_dict['0'] = '0'
-        return result_dict
-    
-    def printData(DATA_DIC):
-        printlist.configure(state='normal')
-        printlist.delete("0.0","end")
-        if DATA_DIC.keys() == 0:
-            printlist.insert("end","해당 달에 일어난 사건이 없습니다.")
-        else:
-            for title, event in DATA_DIC.items():
-                printlist.insert("end","사건 이름: ")
-                printlist.insert("end",title or "")
-                printlist.insert("end","\n")
-                printlist.insert("end","사건 내용: ")
-                printlist.insert("end",event or ""+"\n\n")
-                printlist.configure(state='disabled')
-
-
-
-    def upload_xml():
-        url = 'http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo'
-        service_key = "Nt9JLubbJypY+2kCOW3BhPuQjwuYm0o7eHRKSvBxk7aUk/CI9KgBzv06qHctRy26s56jTjqI0kdOKj1gy3oI8Q=="
-        queryParams = {'serviceKey' : service_key, 'solYear' : Year.get(), 'solMonth' : month.get()}
-        response = requests.get(url, params=queryParams)
-        xmldata = response.content
-
-        Data_Dic = parse_xml(xmldata)
-        printData(Data_Dic)
-
-
-
-    
-
-
-    subsearchwindow = Toplevel(main.window)
+    subsearchwindow = Toplevel()
     subsearchwindow.title("과거 천체 현상 검색")
     subsearchwindow.geometry('250x400')
 
@@ -72,26 +70,13 @@ def pastsearch():
     Mlable = Label(subsearchwindow, text="월(Month)",font=("돋음", 10))
     Mlable.grid(column=1, row=1)
     
-    go = Button(subsearchwindow, text="검색", command=upload_xml)
+    go = Button(subsearchwindow, text="검색", command=lambda: upload_xml(Year, month, printlist))
     go.grid(column=0,row=2)
+    copyButton = Button(subsearchwindow, text="복사하기", command=lambda: listcopy(printlist))
+    copyButton.grid(column=1,row=2)
 
     printlist = Text(subsearchwindow,width=35,height=50, wrap="word")
     printlist.place(x=0,y=100)
     printlist.configure(state='disabled')
     
     subsearchwindow.mainloop()
-
-
-
-
-
-
-    
-    
-    
-    
-
-    
-    
-        
-    
---
+++ search.py
@@ -0,0 +1,93 @@
+#pip install clipboard
+import clipboard
+import requests
+import xml.etree.ElementTree as ET
+from tkinter import *
+
+def XML_parse():
+    url = 'https://api.odcloud.kr/api/15067819/v1/uddi:bab0fa12-d7d7-4e47-975c-e35d424ae165?page=1&perPage=10&returnType=XML'
+    service_key = "/ruEDWbFRa8SMf4ev1DPLwU307V7q7mQg9PPlx7euo7NQ+ktMmwxuICWtFUwJN8BKLv+HpKoHipHpOdeVE6qCw=="
+    queryParams = {'serviceKey': service_key}
+    response = requests.get(url, params=queryParams)
+
+    root = ET.fromstring(response.text)
+    results = []
+    for item in root.findall('data/item'):
+        result = {}
+        for col in item.findall('col'):
+            name = col.attrib['name']
+            value = col.text
+            result[name] = value
+        results.append(result)
+    return results
+
+def print_dictionary(data, value):
+    for dictionary in data:
+        for key, inner_value in dictionary.items():
+            if inner_value == value:
+                temp_dict = dictionary  # 주어진 값이 있는 딕셔너리를 임시 변수에 저장
+                return temp_dict
+    print("없는 별지리")
+
+
+def listcopy(printlist):
+    text_value = printlist.get("1.0", "end-1c")
+    clipboard.copy(text_value)
+
+
+def printResult(findInfo, printlist):
+    datalist = []
+
+    name = findInfo['TITLE']                #이름
+    season = findInfo['D_SUMMARY']          #별이 뜨는 계절
+    position = findInfo['POSITION']         #위치
+    engName = findInfo['SCIENTIFIC_NM']     #영어이름
+    starcount = findInfo['STAR_COUNT']      #별자리를 이루는 별 개수
+
+    datalist.append(name)
+    datalist.append(season)
+    datalist.append(position)
+    datalist.append(engName)
+    datalist.append(starcount)
+
+    printlist.configure(state='normal')
+    printlist.delete("0.0","end")
+
+    printlist.insert("end","별자리 이름: ")
+    printlist.insert("end",datalist[0]+"\n")
+    printlist.insert("end","보이는 계절: ")
+    printlist.insert("end",datalist[1]+"\n")
+    printlist.insert("end","위치: ")
+    printlist.insert("end",datalist[2]+"\n")
+    printlist.insert("end","영어 이름: ")
+    printlist.insert("end",datalist[3]+"\n")
+    printlist.insert("end","별 개수: ")
+    printlist.insert("end",datalist[4]+"\n\n")
+    printlist.configure(state='disabled')
+
+def confirm(input_text, printlist, StarList):
+    in_text = "검색어 : " + input_text.get()
+    findloc = print_dictionary(StarList, input_text.get())
+    printResult(findloc, printlist)
+
+def Search(window):
+    searchwindow = Toplevel(window)
+    searchwindow.title("별자리 검색")
+    searchwindow.geometry('250x200')
+
+    label = Label(searchwindow, text="별자리를 찾아볼까요?",font=("돋음", 10))
+    label.grid(column=0, row=0)
+
+    input_text = Entry(searchwindow, width=30)
+    input_text.grid(column=0, row=2)
+
+    button = Button(searchwindow, text="확인", command=lambda: confirm(input_text, printlist, StarList))
+    button.grid(column=1, row=2)
+    copyButton = Button(searchwindow, text="복사하기", command=lambda: listcopy(printlist))
+    copyButton.grid(column=0,row=3)
+
+    printlist = Text(searchwindow,width=35,height=50)
+    printlist.place(x=0,y=80)
+    printlist.configure(state='disabled')
+
+    StarList = XML_parse()
